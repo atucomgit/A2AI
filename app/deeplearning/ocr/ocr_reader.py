813d68f1e404c3ea5c1fa659f1858e7f40bee788
atucomgit <atucom@mac.com>
2023-05-17 18:37:06 +0900

    ocr_reader.pyを追加

--- app/deeplearning/ocr/ocr_reader.py
+++ app/deeplearning/ocr/ocr_reader.py
@@ -18,4 +18,5 @@ pixel_values = processor(images=image, return_tensors="pt").pixel_values
 generated_ids = model.generate(pixel_values)
 generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
+print("読み取り結果")
 print(generated_text)
