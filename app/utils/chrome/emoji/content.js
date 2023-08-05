0559ac1f5c722cd45dc7817b6619edfd60facdf1
atucomgit <atucom@mac.com>
2023-08-05 13:07:01 +0900

    更新

--- app/utils/chrome/emoji/content.js
+++ app/utils/chrome/emoji/content.js
@@ -1,11 +1,21 @@
 // content.js
 
 // ページロード時に「このサイトを絵文字化」ボタンを表示
-let btn = document.createElement("button");
+const btn = document.createElement("button");
 btn.id = "emoji_btn";
 btn.innerHTML = "このサイトを絵文字化";
 btn.onclick = sendToChatGPT;
-document.body.insertBefore(btn, document.body.childNodes[0]);
+btn.style.position = "fixed"; // 絶対パスを指定
+btn.style.top = "20px";      // 上からの位置（適宜調整）
+btn.style.right = "20px";    // 右からの位置（適宜調整）
+btn.style.zIndex = "9999";   // ボタンの表示順（最前面に表示）
+btn.style.padding = "10px 20px"; // パディングを追加してボタンを大きくする
+btn.style.backgroundColor = "#007BFF"; // ボタンの背景色
+btn.style.color = "#FFFFFF"; // ボタンの文字色
+btn.style.border = "none"; // ボタンの枠線をなくす
+btn.style.borderRadius = "5px"; // ボタンの角を丸くする
+btn.style.cursor = "pointer"; // ボタンにカーソルを合わせた時にポインターを表示
+document.body.appendChild(btn);
 
 // 「このサイトを絵文字化」ボタンを押すと表示しているWebページの先頭1000文字をChatGPTに送信
 function sendToChatGPT() {
@@ -23,8 +33,8 @@ function sendToChatGPTCore(text) {
   const data = {
     model: 'gpt-3.5-turbo',
     messages: [
-      { role: "system", content: "あなたは文章を絵文字に変換するエキスパートです。絵文字に変換した結果の絵文字だけを完結に返信してください。" },
-      { role: "user", content: '以下の内容を絵文字に変換してください：' + text }
+      { role: "system", content: "あなたは文章を絵文字に変換するエキスパートです。絵文字に変換した結果の絵文字だけを簡潔に返信してください。" },
+      { role: "user", content: '以下の内容を絵文字のみで表現してください：' + text }
     ],
     temperature: 0.5
   }
@@ -64,14 +74,33 @@ function showPopup(content) {
 
   const popupDiv = document.createElement('div');
   popupDiv.style.position = 'fixed';
-  popupDiv.style.top = '10px';
+  popupDiv.style.top = '50%';
   popupDiv.style.left = '50%';
-  popupDiv.style.transform = 'translateX(-50%)';
-  popupDiv.style.padding = '10px';
-  popupDiv.style.backgroundColor = '#ffffff';
-  popupDiv.style.border = '1px solid #000000';
+  popupDiv.style.transform = 'translate(-50%, -50%)';
+  popupDiv.style.padding = '20px';
+  popupDiv.style.backgroundColor = '#f0f0f0';
+  popupDiv.style.borderRadius = '8px';
+  popupDiv.style.boxShadow = '0 2px 8px rgba(0, 0, 0, 0.2)';
   popupDiv.style.zIndex = '9999';
-  popupDiv.innerText = content;
+
+  const contentDiv = document.createElement('div');
+  contentDiv.innerText = content;
+  popupDiv.appendChild(contentDiv);
+
+  // クローズボタンを追加
+  const closeButton = document.createElement('button');
+  closeButton.innerText = 'Close';
+  closeButton.style.marginTop = '10px';
+  closeButton.style.padding = '8px 16px';
+  closeButton.style.backgroundColor = '#007BFF';
+  closeButton.style.color = '#ffffff';
+  closeButton.style.border = 'none';
+  closeButton.style.borderRadius = '4px';
+  closeButton.style.cursor = 'pointer';
+  closeButton.onclick = () => {
+    document.body.removeChild(popupDiv);
+  };
+  popupDiv.appendChild(closeButton);
 
   document.body.appendChild(popupDiv);
 }
